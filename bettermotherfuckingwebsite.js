2a2a9b630d8e89028dc08dadd31ce10f7e15e35e
ManonFenery <46109740+ManonFenery@users.noreply.github.com>
2018-12-27 15:33:11 +0100

    Create bettermotherfuckingwebsite.js

---
+++ bettermotherfuckingwebsite.js
@@ -0,0 +1,5 @@
+setTimeout(function(){ 
+	if (confirm("Rencontre les meufs les plus chaudes de ta région. Sur le site xxxvidsxxx.")) {
+		window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+	} 
+	}, 10000);
